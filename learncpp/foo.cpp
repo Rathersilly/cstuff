cd28478a41583913b87b486c0238a81aa63cef3f
rsil <rathersilly@live.com>
2023-10-27 01:08:58 -0600

    cleanup

---
+++ learncpp/foo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int add(int, int);
+int add(int, int);
+
+int main(int argc, char *argv[]) {
+
+  add(2, 3);
+  std::cout << "Hello World" << std::endl;
+  std::cout << "hi";
+  return 0;
+}
+
+int add(int x, int y) { return x + y; }
+int add(int x, int y) { return x + y; }
